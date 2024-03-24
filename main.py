7eb09b9e02331a74c074ed56049066f3673be963
dev-HIten <161627751+dev-HIten@users.noreply.github.com>
2024-03-24 14:47:34 +0530

    j

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-'''My second streamlit project'''
-
 import streamlit as st
-
-st.title('I am working on something else')
-st.subheader('Thank You for checking out my project')
+st.title('About me')
+st.write('''Hello! I'm Hiten, 
+         a programmer with a passion for building creative applications and solving problems logically.''')
+st.header("What I Do")
+st.write('''I'm currently a dropper learning about python,
+          a programmer working on web applications, Backend.''')
+st.header("Connect with Me")
+st.write('''I'm always interested in connecting with other programmers!''')
+st.write('''You can find me on https://github.com/dev-HIten''')
--- pages/YT download.py
+++ pages/YT download.py
@@ -8,26 +8,20 @@ st.caption("Longer video takes time to load,please be patience")
 link = st.text_input("Enter the YouTube video URL: ",placeholder="paste here",value="https://www.youtube.com/watch?v=tPEE9ZwTmy0&pp=ygUZc21hbGxlc3QgdmlkZW8gb24geW91dHViZQ%3D%3D")
 
 if link:
-    buffer = io.BytesIO()
+    video = io.BytesIO()
     try:
         yt = YouTube(link)
-        st.subheader(yt.title)
-        thumbnail_url = yt.thumbnail_url
-        st.image(thumbnail_url, caption=yt.title, use_column_width=True)
-
+        st.video(link)
         stream = yt.streams.get_highest_resolution()
-        stream.stream_to_buffer(buffer)
-        col1 , col2 ,col3 = st.columns([1,1,1])
-        video=buffer.getvalue()
+        stream.stream_to_buffer(video)
+        video=video.getvalue()
+        col1 , col2 = st.columns([1,1])
         with col1:
                 st.download_button(label='Download Video', data=video, file_name='video.mp4', mime='video/mp4')
         with col2:
               preview=st.button(label="Watch Preview")
-        with col3:
-                st.download_button(label="Download Thumbnail", data=thumbnail_url, file_name="thumbnail.png",mime="image/png")
         if preview:
-              st.video(video)
-       
+              st.video(video)       
     except:
         st.warning("Please, Enter correct youtube link")        
     
