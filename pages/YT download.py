efd59ec2d866d86e77220c8e4c011d55a3d6b15a
dev-HIten <161627751+dev-HIten@users.noreply.github.com>
2024-03-24 18:55:22 +0530

    a

--- pages/YT download.py
+++ pages/YT download.py
@@ -15,13 +15,9 @@ if link:
         stream = yt.streams.get_highest_resolution()
         stream.stream_to_buffer(video)
         video=video.getvalue()
-        col1 , col2 = st.columns([1,1])
-        with col1:
-                st.download_button(label='Download Video', data=video, file_name='video.mp4', mime='video/mp4')
+        col1 , col2 ,col3 = st.columns([1,1,1])
         with col2:
-              preview=st.button(label="Watch Preview")
-        if preview:
-              st.video(video)       
+                st.download_button(label='Download Video', data=video, file_name='video.mp4', mime='video/mp4')
     except:
         st.warning("Please, Enter correct youtube link")        
     
